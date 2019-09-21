8df6349193b7cbbd66c4c1630dba39baba427493
Tyler Biswell <tsbiswell@gmail.com>
2019-09-21 10:03:52 -0600

    MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -22,6 +22,17 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        // console.log(res.data);
+        updateColors([
+          ...colors.filter(color => color.id !== colorToEdit.id),
+          res.data,
+        ]);
+        setEditing(false);
+      })
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
