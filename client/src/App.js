21428f6b107bc6e7bb37b2473dafbe2c45f1df47
Tyler Biswell <tsbiswell@gmail.com>
2019-09-21 09:58:50 -0600

    built login form to authenticate users

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
+import './styles.scss';
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
+      <div className='App'>
+        <Link to='/'>Login</Link>
+        <Link to='/bubblepage'>Bubble Page</Link>
+        <Route exact path='/' component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path='/bubblepage' component={BubblePage} />
       </div>
     </Router>
   );
